833fbb6baf193709e113bee1e5e7dfee9fe922c5
Felipe Bailez <bailez@usp.br>
2020-06-24 17:25:16 -0300

    adiciona os comentarios organizando listas de exercicios

---
+++ monitoria/revisao.R
@@ -0,0 +1,20 @@
+# Lista 1 - Introdução
+
+# Lista 2 - Lendo arquivos e utilizando Rstudio
+
+# Lista 3 - Vetores e listas
+
+# Lista 4 - Matrizes
+
+# Lista 5 - DataFrames
+
+# Lista 6 - DataFrames
+
+# Lista 7/8 - if Else, While e funções
+
+# Lista 9 - For Loop
+
+# Lista 10 - Operadores Lógicos
+
+# Lista 11 - Loops
+
