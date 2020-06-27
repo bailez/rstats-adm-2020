43fa2ff97339d849dd7bedcd1059636e6e6cc983
Felipe Bailez <bailez@usp.br>
2020-06-27 16:16:52 -0300

    prova resolucao

--- prova/prova2020.script.R
+++ prova/prova2020.script.R
@@ -14,25 +14,22 @@
 # Q2. soma de todos os elementos dos vetores - eliminar os não definidos
   soma_vetores <- function(vet){
     soma <- 0
- 
-    # completar
-    
+    for (i in vet){
+      if(!is.na(i)){
+        soma <- soma + i
+      }
+    }
     return(soma)
   }
   
   soma_v2 <- soma_vetores(v2)
   print(v2)
   print(soma_v2)
-
   
-    
-# Q3. soma de dois vetores do mesmo tamanho
-#                           
+# Q3. soma de dois vetores do mesmo tamanho                           
   soma_2vetores_tam_igual <- function(vet1,vet2){
     vresult <- NULL
-    
-    # completar
-    
+    vresult = vet1 + vet2
     return(vresult)
   }
   
@@ -41,29 +38,46 @@
   print(soma_vets)
 
 # Q4. soma de dois vetores - considerar zeros quando um vetor for menor que o outro
-#
   soma_2vetores_zeros <- function(vet1,vet2){
     vresult <- NULL
-    
-    # completar
-    
+    len = length(vet1) - length(vet2)
+    if (len > 0){
+      print("vet1 é maior")
+      vet2 = c(vet2, rep(0,len))
+    } 
+    else if (len < 0){
+      print("vet2 é maior")
+      vet1 = c(vet1, rep(0,-len))
+    }
+    else{
+     print("vets são iguais") 
+    }
+    vresult = vet1 + vet2
     return(vresult)
   }
-  
   
   soma_vets <- soma_2vetores_zeros(v1,v2)
   print(v1)
   print(v2)
   cat("Soma dos vetores \n" )
   print(soma_vets)
-  
-# Q5. soma de dois vetores - soma feita até o tamanho do vetor menor  
-#                           desprezar os outros elementos do vetor maior
+    
+# Q5. soma de dois vetores - soma feita até o tamanho do vetor menor desprezar os outros elementos do vetor maior
   soma_2vetores_ate_tammenor <- function(vet1,vet2){
     vresult <- NULL
-    
-    # completar
-    
+    len = length(vet1) - length(vet2)
+    if (len > 0){
+      print("vet1 é maior")
+      vet1 = vet1[1:length(vet2)]
+    } 
+    else if (len < 0){
+      print("vet2 é maior")
+      vet2 = vet2[1:length(vet1)]
+    }
+    else{
+      print("vets são iguais") 
+    }
+    vresult = vet1 + vet2
     return(vresult)
   }
   
@@ -72,14 +86,23 @@
   print(v2)
   cat("Soma dos vetores \n" )
   print(soma_vets)
-  
         
 # Q6. soma de dois vetores - repetir valores se um vetor for menor que o outro
   soma_2vetores_repete <- function(vet1,vet2){
     vresult <- NULL
-    
-    # completar
-    
+    len = length(vet1) - length(vet2)
+    if (len > 0){
+      print("vet1 é maior")
+      vet2 = c(vet2,vet2[1:len])
+    } 
+    else if (len < 0){
+      print("vet2 é maior")
+      vet1 = c(vet1,vet1[1:-len])
+    }
+    else{
+      print("vets são iguais") 
+    }
+    vresult = vet1 + vet2
     return(vresult)
   }
   
